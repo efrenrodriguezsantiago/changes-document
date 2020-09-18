1f00fad4cc8b326be4f2d3eb5562f98f28ee22a6
CONTA-47 <CONTA-47@DESKTOP-RQ7SNBR>
2020-09-17 19:54:40 -0500

    leer un numero hasta que se introduzca 0 indicar si es par o impar

---
+++ src/ciclo_for.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ciclo_for {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		//incremento
+		for(int i=1;i<10;i++) {
+			System.out.println(i);
+		}
+		//decremento
+		
+		for(int j=1;j>=1;j--) {
+			System.out.println(j);
+		}
+		//------------------------------
+		//cuantos numeros quiere impromir
+		int contador;
+		
+		System.out.println("digite la cantidad de terminos: ");
+		contador = entrada.nextInt();
+		
+		
+		for(int l=1;l<=contador;l+=2) {//numeros pares
+		
+			System.out.println(l);
+	}
+	}
+
+}
